3d4f804c17b17d5b510ec250aea151a2d02d2281
sajay <sajaykannan10@gmail.com>
2025-08-06 14:41:10 +0530

    otp generated

---
+++ diet_app/sample.py
@@ -0,0 +1,6 @@
+
+
+from random import randint
+
+otp = randint(10000,100000)
+
--- diet_app/views.py
+++ diet_app/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.views.generic import View
 
 from diet_app.forms import SignUpForm
+
+from diet_app.models import UserOtp
+
+from django.contrib import messages
+
+from random import randint
+
+def generate_otp(user_id):
+
+    otp = randint(10000,100000)
+
+    otp+=user_id
+
+    return str(otp)
+
+
+
+
+
+
 
 
 
@@ -17,5 +37,48 @@ class SignUpView(View):
         form_instance = self.form_class()
 
         return render(request,self.template_name,{"form":form_instance})
+    
+    def post(self,request,*args,**kwargs):
+
+        form_data = request.POST
+
+        form_instance = self.form_class(form_data)
+
+        if form_instance.is_valid():
+
+            user_instance = form_instance.save(commit=False)
+
+            user_instance.is_active = False
+
+            user_instance.save()
+
+            otp = generate_otp(user_instance.id)
+
+            UserOtp.objects.create(owner=user_instance,otp=otp)
+
+            # logic for sending email
+            # phone #333
+
+            messages.success(request,"otp has been send ....")
+
+            return redirect("register")
+        
+        messages.error(request,"failed to create account ")
+        return render(request,self.template_name,{"form":form_instance})
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
 
 
