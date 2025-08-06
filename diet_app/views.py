4358355a32ced583b2413d446fd2022fdb3a32ab
sajay <sajaykannan10@gmail.com>
2025-08-06 13:51:47 +0530

    resgister get

--- diet_app/views.py
+++ diet_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import View
+
+from diet_app.forms import SignUpForm
+
+
+
+class SignUpView(View):
+
+    template_name = "register.html"
+
+    form_class = SignUpForm
+
+    def get(self,request,*args,**kwargs):
+
+        form_instance = self.form_class()
+
+        return render(request,self.template_name,{"form":form_instance})
+
+
